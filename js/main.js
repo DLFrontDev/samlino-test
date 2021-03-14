2b69393eb5733970f658cbde61b6a7c75f5bd6ec
DESKTOP-EKPNVAP\Dlcra <diogocardosoluis@gmail.com>
2021-03-14 23:37:38 +0000

    Rename main.js to router.js

--- js/main.js
+++
@@ -1,8 +0,0 @@
-// Check SessionStorage for token and current location
-if (!window.location.pathname.includes('profile') && window.sessionStorage.getItem('token')) {
-    // Replace location with profile page if user is logged in
-    window.location.replace(window.location.href + 'profile');
-} else if(window.location.pathname.includes('profile') && !window.sessionStorage.getItem('token')) {
-    // Replace location with login page if profile page is accessed without login
-    window.location.replace(window.location.origin + window.location.pathname.replace('profile', ''));
-}
